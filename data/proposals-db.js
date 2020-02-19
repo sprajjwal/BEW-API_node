921dcca5d143b2c326c4d26cf41bbb52fdd74d18
sprajjwal <shashwatprajjwal@gmail.com>
2020-02-19 15:31:22 -0800

    mongo

--- data/proposals-db.js
+++ data/proposals-db.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 const assert =  require("assert")
 
 const url = "mongodb://localhost:27017/proposals-db";
+const heroku_url = "mongodb://<dbuser>:<dbpassword>@ds229118.mlab.com:29118/heroku_4gm3n5zp"
 mongoose.Promise = global.Promise;
 mongoose.connect(
-  url,
+  heroku_url,
   {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 );
 mongoose.connection.on("error", console.error.bind(console, "MongoDB connection Error:"));
