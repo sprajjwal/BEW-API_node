30c71562e2d0d34a8eecbffb2561857fa9e7fe33
sprajjwal <shashwatprajjwal@gmail.com>
2020-02-26 23:00:50 -0800

    port added

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ require("./controllers/users")(app);
 require("./controllers/proposals")(app);
 
 app.get('/', (req, res) => { res.send("hello world")})
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT ||port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app;
